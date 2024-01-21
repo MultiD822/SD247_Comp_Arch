058b7f25a5b585835f846565f17c0c91620147d4
Your Name <kaischenkel@gmail.com>
2024-01-21 14:04:21 -0800

    Adding the readme file and made main in the C file

--- Project 1/GuessingGame.c
+++ Project 1/GuessingGame.c
@@ -1,4 +1,20 @@
 /*
-Programmer: Kai Schenkel
-Data: 1/21/2024
+- Programmer: Kai Schenkel
+- Class: SD-247 Computer Architecture
+- Data: 1/21/2024
+- Project 1: Introduction to C / Guessing Game 
+- Sources: KS_GuessingGame.java, Book from class: Computer Systems
+- Objective: This C program is meant to be a Guess the Number game, where our program will generate a random number between a explicit range like 1 to 100.
+    Then the play will make a guess and see if it matchs the random number set by the computer.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int guessingNumber;
+    int guessMade;
+
+    return 0;
+}
---
+++ Project 1/README.md
@@ -0,0 +1,7 @@
+# Project_1_Introduction_to_C
+Objective: This C program is meant to be a Guess the Number game, where our program will generate a random number between a explicit range like 1 to 100.
+    Then the play will make a guess and see if it matchs the random number set by the computer.
+
+Sources: 
+   -One of the sources i used was my KS_GuessingGame.java which i made for my CS141 class
+   -I used this one Youtube video from Bro Code link: https://youtu.be/maVv62OPjPs?si=cTsdbcG8Ch6UpmTg
