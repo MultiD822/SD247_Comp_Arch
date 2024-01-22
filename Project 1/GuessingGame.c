adb9abfca3309b4870a998d3c3bfed312436be49
Your Name <kaischenkel@gmail.com>
2024-01-21 16:43:32 -0800

    Updateing Sources

--- Project 1/GuessingGame.c
+++ Project 1/GuessingGame.c
@@ -3,7 +3,7 @@
 - Class: SD-247 Computer Architecture
 - Data: 1/21/2024
 - Project 1: Introduction to C / Guessing Game 
-- Sources: KS_GuessingGame.java, Book from class: Computer Systems
+- Sources: KS_GuessingGame.java, Book from class: Computer Systems, Bro code on Youtube and Geeks for Geeks
 - Objective: This C program is meant to be a Guess the Number game, where our program will generate a random number between a explicit range like 1 to 100.
     Then the play will make a guess and see if it matchs the random number set by the computer.
 */
@@ -94,7 +94,7 @@ int difficultyLevel() {
     return MAX;
 }
 
-void gameIntro(){
+void gameIntro() {
     printf("[]==========================================================================[]\n");
     printf("[] Wellcome to the Nubmer Guessing Game, The computer will grenert a random []\n");
     printf("[] number from 1 to the level of difficulty that you will select from.      []\n");
--- Project 1/README.md
+++ Project 1/README.md
@@ -5,4 +5,5 @@ Objective: This C program is meant to be a Guess the Number game, where our prog
 Sources: 
    -One of the sources i used was my KS_GuessingGame.java which i made for my CS141 class
    -I used this one Youtube video from Bro Code link: https://youtu.be/maVv62OPjPs?si=cTsdbcG8Ch6UpmTg
-   -I also looked at Geeks for Geeks link: https://www.geeksforgeeks.org/number-guessing-game-in-c/
+   -I also looked at Geeks for Geeks link: https://www.geeksforgeeks.org/number-guessing-game-in-c
+   -The book for the class Computer Systems 2.2 and 2.3
