da05bcc7a30e0b420c4cee1f542a11c643e859e9
Your Name <kaischenkel@gmail.com>
2024-02-21 21:49:13 -0800

    Finishing the game

--- Project 3/Battle_Ship/battleship.c
+++ Project 3/Battle_Ship/battleship.c
@@ -1,17 +1,23 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
 #include <string.h>
 /*
-
+- Programmer: Kai Schenkel
+- Class: SD-247 Computer Architecture
+- Data: 2/21/2024
+- Project 3: Implementing Battleship in C
+- Sources: Book from class: Computer Systems, Bro code on Youtube and Geeks for Geeks
+- Objective: This program is a simple implementation of the game Battleship. The game is played on a 10x10 grid. 
+    The game is played between the user and the computer. The user will place their ships on the board and then the computer will place their ships on the board. 
+    The user will then take a turn and guess where the computer's ships are. The computer will then take a turn and guess where the user's ships are. 
+    The game will continue until all the ships are sunk. The game will then display who won the game.
 */
 #define BOARD_SIZE 10
 #define NUM_SHIPS 5
-#define SHIP_LENGTHS \
-  {                  \
-    5, 4, 3, 3, 2    \
-  }
+#define SHIP_LENGTHS{5, 4, 3, 3, 2}
 
 typedef struct
 {
@@ -40,21 +46,16 @@ void initializeBoard(char board[BOARD_SIZE][BOARD_SIZE])
 }
 void printEdge()
 {
-  // print the edge of the board
-  char wall = '=';
-  printf("[]");
-  for (int i = 0; i < 20; i++)
-  {
-    printf("%c", wall);
-  }
-  printf("[]\n");
+  printf("[]===|===|===|===|===|===|===|===|===|===[]\n");
 }
+
 void displayBoard(char board[BOARD_SIZE][BOARD_SIZE])
 {
   // display the board
   //printEdge();
-  printf("[]===|===|===|===|===|===|===|===|===|===[]\n");
+  printEdge();
   printf("[] A | B | C | D | E | F | G | H | I | J []\n");
+  printEdge();
   for (int i = 0; i < BOARD_SIZE; i++) {
     printf("[]");
     for (int j = 0; j < BOARD_SIZE; j++){
@@ -66,17 +67,46 @@ void displayBoard(char board[BOARD_SIZE][BOARD_SIZE])
     }
     printf("[]\n");
   }
-  printf("[]===|===|===|===|===|===|===|===|===|===[]\n");
+  printEdge();
 }
 
-bool isValidPlacement(char board[BOARD_SIZE][BOARD_SIZE], Ship shipToPlace, Coordinate start, Coordinate end)
+bool isValidPlacement(char board[BOARD_SIZE][BOARD_SIZE], Ship shipToPlace, char shipDirection)
 {
   // logic to check if a ship placement is valid
   //If the ship overlaps with another ship then return false
   //If the ship is not horizontal or vertical then return false
   //If the ship is not the correct length then return false
   //If the ship placement is not valid then return false
+  if (shipToPlace.start.x < 0 || shipToPlace.start.x >= BOARD_SIZE || shipToPlace.start.y < 0 || shipToPlace.start.y >= BOARD_SIZE){
+    return false;
+  }
+  //If the ship is horizontal then we need to check if the ship is going off the board
+  if (shipDirection == 'H') {
+    if(shipToPlace.start.x + shipToPlace.length > BOARD_SIZE){
+      return false; //ship is going off the board
+      //Then we need to check if the ship will over lap with another ship
+      for (int i = 0; i < shipToPlace.length; i++){
+        if(board[shipToPlace.start.x][shipToPlace.start.y] != 0){
+        return false; //overlapping
+        }
+      }
+    }
+  } else if (shipDirection == 'V') {
+    if(shipToPlace.start.y + shipToPlace.length > BOARD_SIZE){
+      return false; //ship is going off the board
+      //Then we need to check if the ship will over lap with another ship
+      for (int i = 0; i < shipToPlace.length; i++){
+        if(board[shipToPlace.start.x][shipToPlace.start.y] != 0){
+        return false; //overlapping
+        }
+      }
+    }
 
+  } else {
+    return false; //ship is not horizontal or vertical
+  }
+  
+  return true;
 }
 
 bool isValidGuess(char board[BOARD_SIZE][BOARD_SIZE], Coordinate guess)
@@ -91,123 +121,193 @@ bool isValidGuess(char board[BOARD_SIZE][BOARD_SIZE], Coordinate guess)
   return false;
 }
 
-void placeShip(char board[BOARD_SIZE][BOARD_SIZE])
-{
-  
+void placeShips(char board[BOARD_SIZE][BOARD_SIZE], Ship ships[]){
   // place the ship on the board
   //We need to place the ship on the board by setting the start and end coordinates to 'S'
   //When we hit the start and end coordinates we need to check if the ship is horizontal or vertical
   //If the ship is horizontal then we need to set the start x coordinate to 'S' and the end x coordinate to 'S'
   //If the ship is vertical then we need to set the start y coordinate to 'S' and the end y coordinate to 'S'
-  Ship ship;
-  printf("Which ship do you want to place?\n");
-  printf("1. Carrier (5)\n");
-  printf("2. Battleship (4)\n");
-  printf("3. Cruiser (3)\n");
-  printf("4. Submarine (3)\n");
-  printf("5. Destroyer (2)\n");
-  int shipChoice;
-  scanf("%d", &shipChoice);
-  switch (shipChoice){
-    case 1:
-      ship.length = 5;
-      break;
-    case 2:
-      ship.length = 4;
-      break;
-    case 3:
-      ship.length = 3;
-      break;
-    case 4:
-      ship.length = 3;
-      break;
-    case 5:
-      ship.length = 2;
-      break;
-
-    default:
-      printf("Invalid choice\n");
-      char stillWantToPlay;
-      printf("Do you still want to play? (Y/N)\n");
-      scanf("%c", &stillWantToPlay);
-      if (stillWantToPlay == 'N')
-      {
-        exit(0);
-      }
-      break;
-  }
-  //Now we ask the user if they want to place the ship horizontally or vertically
-  printf("Do you want to place the ship horizontally or vertically?\n");
-
   
+  for (int shipIndex = 0; shipIndex < NUM_SHIPS; shipIndex++) {
+    displayBoard(board);
+    printf("Placing ship %d of length %d\n", shipIndex + 1, ships[shipIndex].length);
+    // Ask the user for the orientation of the ship
+    printf("Do you want to place the ship horizontally or vertically? (Enter 'H' for Horizontal, 'V' for Vertical):\n");
+    char orientation;
+    scanf(" %c", &orientation);
+        
+    // Ask the user for the starting coordinate of the ship
+    printf("Enter the starting coordinate for the ship (x y): \n");
+    scanf(" %d", &ships[shipIndex].start.x);
+    scanf (" %d", &ships[shipIndex].start.y);
 
-  printf("Enter 'H' for Horizontal\n");
-  printf("Enter 'V' for Vertical\n");
-  char playChoice;
-    scanf("%c", &playChoice);
-
-  switch (playChoice){
-    case 'H':
-      //If the ship is horizontal then we need to set the start x coordinate to 'S' and the end x coordinate to 'S'
-      printf("Enter the start x coordinate\n");
-      scanf("%d", &ship.start.x);
-      printf("Enter the end x coordinate\n");
-      scanf("%d", &ship.end.x);
-      for (int i = ship.start.x; i < ship.end.x; i++)
-      {
-        board[i][ship.start.y] = 'S';
-      }
-      printf("The ship has been placed\n");
-      break;
-    case 'V':
-      //If the ship is vertical then we need to set the start y coordinate to 'S' and the end y coordinate to 'S'
-      printf("Enter the start y coordinate\n");
-      scanf("%d", &ship.start.y);
-      printf("Enter the end y coordinate\n");
-      scanf("%d", &ship.end.y);
-
-      for (int i = ship.start.y; i < ship.end.y; i++)
-      {
-        board[ship.start.x][i] = 'S';
-      }
-      break;
-
-    default:
-      printf("Invalid choice\n");
-      char stillWantToPlay;
-      printf("Do you still want to play? (Y/N)\n");
-      scanf("%c", &stillWantToPlay);
-      if (stillWantToPlay == 'N')
-      {
-        exit(0);
-      }
-      break;
+    // Check if the placement is valid
+    if (isValidPlacement(board, ships[shipIndex], orientation)) {
+            // Place the ship on the board
+            if (orientation == 'V') {
+                for (int i = 0; i < ships[shipIndex].length; i++) {
+                    board[ships[shipIndex].start.x + i][ships[shipIndex].start.y] = 'S';
+                }
+            } else { // 'V'
+                for (int i = 0; i < ships[shipIndex].length; i++) {
+                    board[ships[shipIndex].start.x][ships[shipIndex].start.y + i] = 'S';
+                }
+            }
+            printf("Ship placed.\n");
+    } else {
+      printf("Invalid placement. Please try again.\n");
+      shipIndex--; // Decrement the index to retry
+    }
   }
 }
+
+//Place Ships randomly
+void placeShipsRandomly(char board[BOARD_SIZE][BOARD_SIZE], Ship ships[]) {
+    srand(time(NULL)); // Seed the random number generator
+
+    for (int shipIndex = 0; shipIndex < NUM_SHIPS; shipIndex++) {
+        bool placed = false;
+        while (!placed) {
+            // Randomly choose orientation
+            char orientation = rand() % 2 == 0 ? 'H' : 'V';
+            // Randomly choose starting coordinates
+            if (orientation == 'H') {
+                ships[shipIndex].start.x = rand() % (BOARD_SIZE - ships[shipIndex].length + 1);
+                ships[shipIndex].start.y = rand() % BOARD_SIZE;
+            } else { // 'V'
+                ships[shipIndex].start.x = rand() % BOARD_SIZE;
+                ships[shipIndex].start.y = rand() % (BOARD_SIZE - ships[shipIndex].length + 1);
+            }
+
+            // Check if the placement is valid
+            if (isValidPlacement(board, ships[shipIndex], orientation)) {
+                // Place the ship on the board
+                for (int i = 0; i < ships[shipIndex].length; i++) {
+                    if (orientation == 'H') {
+                        board[ships[shipIndex].start.x + i][ships[shipIndex].start.y] = 'S';
+                    } else { // 'V'
+                        board[ships[shipIndex].start.x][ships[shipIndex].start.y + i] = 'S';
+                    }
+                }
+                placed = true;
+            }
+        }
+    }
+}
+
+
+
+void initializeShips(Ship ships[]) {
+  // initialize the ships
+  //We need to initialize the ships by setting the start and end coordinates to 0
+  //We also need to set the length of the ship
+  //We also need to set the ship to not sunk
+
+  for (int i = 0; i < NUM_SHIPS; i++)
+  {
+    ships[i].start.x = 0;
+    ships[i].start.y = 0;
+    ships[i].end.x = 0;
+    ships[i].end.y = 0;
+    ships[i].isSunk = false;
+  }
+  //We need to set the length of the ships
+  for (int i = 0; i < NUM_SHIPS; i++)
+  {
+    if (i == 0)
+    {
+      ships[i].length = 5;
+    }
+    else if (i == 1)
+    {
+      ships[i].length = 4;
+    }
+    else if (i == 2)
+    {
+      ships[i].length = 3;
+    }
+    else if (i == 3)
+    {
+      ships[i].length = 3;
+    }
+    else if (i == 4)
+    {
+      ships[i].length = 2;
+    }
+  }
+}
+
 
 bool isHit(char board[BOARD_SIZE][BOARD_SIZE], Coordinate guess)
 {
   // check if the guess is a hit
-  if (board[guess.x][guess.y] == 'H')
-  {
+  if (board[guess.x][guess.y] == 'H' || board[guess.x][guess.y] == 'S'){
     return true;
   }
   return false;  
 }
 
-bool isSunk(char board[BOARD_SIZE][BOARD_SIZE], Ship ship)
-{
+bool isSunk(char board[BOARD_SIZE][BOARD_SIZE], Ship ship){
   // check if the ship is sunk
-  if (ship.isSunk == true)
-  {
-    return true;
+  if (ship.start.x == ship.end.x){
+    for(int y = ship.start.y; y <= ship.end.y; y++){
+      if(board[ship.start.x][y] != 'H'){  //If any part of the ship is not hit
+        return false;
+      }
+    }
+  } else if (ship.start.y == ship.end.y){
+    for(int x = ship.start.x; x <= ship.end.x; x++){
+      if(board[x][ship.start.y] != 'H'){  //If any part of the ship is not hit
+        return false;
+      }
+    }
   }
-  return false;
+  return true;
 }
 
-void takeTurn(char board[BOARD_SIZE][BOARD_SIZE], Ship ships[NUM_SHIPS], bool isUserTurn)
+void takeTurn(char shipBoard[BOARD_SIZE][BOARD_SIZE], char guessingBoard[BOARD_SIZE][BOARD_SIZE],  Ship ships[], bool isUserTurn)
 {
   // logic for a turn (user or computer)
+  //If it is the user's turn then we need to ask the user for a guess
+  //If the user's guess is a hit then we need to set the board to 'H'
+  //If the user's guess is a miss then we need to set the board to 'O'
+  //If the user's guess is a hit then we need to check if the ship is sunk
+  //If the ship is sunk then we need to set the ship to sunk
+  //If it is the computer's turn then we need to generate a random guess, if it is a hit do the samw thing as the user's turn
+  Coordinate guess;
+  if (isUserTurn) {
+    printf("Enter your guess (ex. 2 , 5): ");
+    scanf(" %d", &guess.x);
+    scanf(" %d", &guess.y);
+    if(!isValidGuess(shipBoard, guess)){
+      printf("Invalid guess\n");
+      return;
+    }
+  } else {
+    guess.x = rand() % BOARD_SIZE;
+    guess.y = rand() % BOARD_SIZE;
+  }
+
+  // Check if the guess is a hit or miss
+    if (shipBoard[guess.x][guess.y] == 'S') { 
+        printf("%s\n", isUserTurn ? "Hit!" : "The computer hits!");
+        guessingBoard[guess.x][guess.y] = 'H'; // Mark hit on the guessing board
+        shipBoard[guess.x][guess.y] = 'H'; 
+        // Check if the ship is sunk
+        for (int i = 0; i < NUM_SHIPS; i++) {
+            if (ships[i].start.x <= guess.x && ships[i].end.x >= guess.x && ships[i].start.y <= guess.y && ships[i].end.y >= guess.y) {
+                if (isSunk(shipBoard, ships[i])) {
+                    ships[i].isSunk = true;
+                    printf("%s\n", isUserTurn ? "You sunk a ship!" : "The computer sunk your ship!");
+                }
+            }
+        }
+        
+    } else {
+        printf("%s\n", isUserTurn ? "Miss." : "The computer misses.");
+        guessingBoard[guess.x][guess.y] = 'O'; // Mark miss on the guessing board
+    }
+
 }
 
 bool isGameOver(Ship ships[NUM_SHIPS])
@@ -245,10 +345,61 @@ void printMenu()
 
 int main()
 {
-  // some main code
-  char board[BOARD_SIZE][BOARD_SIZE];
-  initializeBoard(board);
-  displayBoard(board);
-  placeShip(board);
-  displayBoard(board);
+  // Initialize the game board
+  char plays_Ship_Board[BOARD_SIZE][BOARD_SIZE];
+  char players_Guess_Board[BOARD_SIZE][BOARD_SIZE];
+    initializeBoard(plays_Ship_Board);
+    initializeBoard(players_Guess_Board);
+  char computers_Ship_Board[BOARD_SIZE][BOARD_SIZE];
+  char computers_Guess_Board[BOARD_SIZE][BOARD_SIZE];
+    initializeBoard(computers_Ship_Board);
+    initializeBoard(computers_Guess_Board);
+   
+
+    // Define and initialize ships
+    Ship playerShips[NUM_SHIPS];
+    initializeShips(playerShips); 
+
+    Ship computerShips[NUM_SHIPS];
+    initializeShips(computerShips); 
+    
+    printMenu();
+    int choice;
+    scanf(" %d", &choice);
+    if (choice == 2)
+    {
+      return 0;
+    }
+    // Place ships on the board
+    placeShips(plays_Ship_Board, playerShips);
+    // Place ships on the board for the computer
+    placeShipsRandomly(computers_Ship_Board, computerShips);
+  
+    // Main game loop
+    bool gameRunning = true;
+    while (gameRunning) {
+    // Player's turn: Make a guess against the computer's ships
+    displayBoard(players_Guess_Board);
+    displayBoard(plays_Ship_Board);
+    takeTurn(computers_Ship_Board, players_Guess_Board, playerShips, true);
+    
+    // Check for game over condition after each turn
+    if (isGameOver(playerShips)) {
+        printf("Player wins!\n");
+        gameRunning = false;
+        break;
+    }
+    printf("Computer's turn: \n");
+    // Computer's turn: Implement logic for the computer to make a guess against the player's ships
+    takeTurn(plays_Ship_Board, computers_Guess_Board, computerShips, false);
+
+    // Again, check for game over condition
+    if (isGameOver(computerShips)) {
+        printf("Computer wins!\n");
+        gameRunning = false;
+    }
+}
+
+
+    return 0;
 }
