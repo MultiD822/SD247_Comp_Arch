04024ac8d0cf91461b2993435f642b7d2b0a4909
Your Name <kaischenkel@gmail.com>
2024-03-15 15:46:26 -0700

    Setting up project folders

---
+++ Project 4/KS_C_Terminal/Program.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+/*
+- Programmer: Kai Schenkel
+- Class: SD-247 Computer Architecture
+- Data: 3/15/2024
+- Project 4: C Terminal
+- Sources: Book from class: Computer Systems, Bro code on Youtube and Geeks for Geeks
+- Objective: The objective of this assignment is to reinforce your understanding of basic C programming concepts 
+    and to enhance your skills in handling input/output operations, string manipulation, and command execution.
+*/
+
+int main() {
+    
+    printf("Hello world");
+    return 0;
+}
